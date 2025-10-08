361079876741efb974a62ce5052d44ca74965b33
albesroger <albescaballero@gmail.com>
2025-10-08 02:35:10 -0400

    se marca la que se toca

--- frontend/general_template/src/app/components/seeTask/changeTaskType/changeTaskType.component.ts
+++ frontend/general_template/src/app/components/seeTask/changeTaskType/changeTaskType.component.ts
@@ -1,17 +1,22 @@
 import { Component, signal } from '@angular/core';
-import {} from '../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-change-task-type',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './changeTaskType.component.html',
 })
 export class ChangeTaskTypeComponent {
   seeTask = signal('listening');
+  isActivate = signal(false);
+
   constructor() {}
 
   changeTaskType(taskType: string) {
     this.seeTask.set(taskType);
     console.log(this.seeTask());
+  }
+  changeActivate() {
+    this.isActivate.set(!this.isActivate());
   }
 }
